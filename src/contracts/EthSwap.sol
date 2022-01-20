14915af94b50b450f71d0966f298949a0d67d459
Adarsh Latiyan <adarshlatiyan@gmail.com>
2022-01-20 13:02:27 +0530

    Implement purchase token functionality Implement test for purchase functionality

--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -1,8 +1,19 @@
 pragma solidity ^0.8.0;
 
+import "./Token.sol";
+
 contract EthSwap {
     string public name = "EthSwap Instant Exchange";
-    constructor() public {
+    Token public token;
+    uint public rate = 100;
 
+    constructor(Token _token) public {
+        token = _token;
+    }
+
+    function buyTokens() public payable {
+        // Amount of ether*rate
+        uint tokenAmount = msg.value * rate;
+        token.transfer(msg.sender, tokenAmount);
     }
 }
--- test/EthSwap.test.js
+++ test/EthSwap.test.js
@@ -9,12 +9,12 @@ function tokens(n) {
     return web3.utils.toWei(n, 'ether')
 }
 
-contract("EthSwap", (accounts) => {
+contract("EthSwap", ([deployer, investor]) => {
     let ethSwap, token
 
     before(async function () {
-        ethSwap = await EthSwap.new()
         token = await Token.new()
+        ethSwap = await EthSwap.new(token.address)
         await token.transfer(ethSwap.address, tokens('1000000'))
     })
 
@@ -34,6 +34,25 @@ contract("EthSwap", (accounts) => {
         it('should have all the token balance', async function () {
             let balance = await token.balanceOf(ethSwap.address)
             assert.equal(balance.toString(), tokens('1000000'))
+        });
+    })
+
+    describe('Buy Tokens', async () => {
+        let result
+
+        before(async function () {
+            // Purchase tokens before each test
+            result = await ethSwap.buyTokens({from: investor, value: tokens('1')})
+        })
+
+        it('should allow users to instantly purchase tokens for fixed price', async function () {
+            // Check investor token balance after purchase
+            let investorBal = await token.balanceOf(investor)
+            assert.equal(investorBal, tokens('100'))
+
+            // Check EthSwap balance after purchase
+            let ethSwapBal = await token.balanceOf(ethSwap.address)
+            assert.equal(ethSwapBal.toString(), tokens('999900'))
         });
     })
 })
